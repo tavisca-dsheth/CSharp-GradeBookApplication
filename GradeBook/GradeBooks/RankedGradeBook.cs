bc2b123f01feb40374bc027c269b3b5333a9b6d4
tavisca-dsheth <dsheth@tavisca.in>
2019-10-31 11:08:00 +0530

    Changes 4.1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,6 +12,16 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
